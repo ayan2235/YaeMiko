3e23d6866ae1938015a6d3ab6e248c01df9426e5
ZION BIOM <102071095+ayan2235@users.noreply.github.com>
2024-02-26 19:50:12 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -14,14 +14,14 @@ class Config(object):
     API_HASH = "80157e31b41407507c6a7849113cc1d4"
 
     # Database configuration (PostgreSQL)
-    DATABASE_URL = "postgres://ierjlkr:OG4dxzO67Zret3Zii43Hhvujkg89WVry0n9KsHE@karma.db.elephantsql.com/ierjlkr"
+    DATABASE_URL = "postgresql://Bot_followwho:6d1a6eb3e414d737afda6a214c5df43c48356bac@pw8.h.filess.io:5432/Bot_followwho"
 
     # Event logs chat ID and message dump chat ID
     EVENT_LOGS = -1002088557628
     MESSAGE_DUMP = -1002088557628
 
     # MongoDB configuration
-    MONGO_DB_URI = "mongodb+srv://t45:t45@cluster0.plfylpo.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_DB_URI = "mongodb+srv://Ayan:TnOx4mKnolqnOf5ZipysFsVg2FfyvvLRyI3srAn6qQjBUb8TjDJUTVfQ4CZM6fCR@cluster0.otxzq7c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
     # Support chat and support ID
     SUPPORT_CHAT = "Noob_xd03"
